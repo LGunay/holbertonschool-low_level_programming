f2235c3e99d9744cfc7e60760032a44967c91753
root <gqasimova93@gmail.com>
2023-10-18 03:37:20 -0700

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - check the code
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int  i, j, a;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			a = i * j;
+			if (a >= 10)
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (a <= 9)
+			{
+				_putchar(a + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
