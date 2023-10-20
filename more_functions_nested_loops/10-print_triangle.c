610af968b9c9254a6500a5483bebbb3563650962
root <gqasimova93@gmail.com>
2023-10-20 09:10:43 -0700

    10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - checks
+ * @size:var
+ *
+ * Return: 1 if upper, otherwise 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; i++)
+		{
+			if (size - i < j)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+	}
+	_putchar('\n');
+}
+
+
