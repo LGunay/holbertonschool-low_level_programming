22ebda477ef73972ad8d954316ef1825f0be9324
root <gqasimova93@gmail.com>
2023-10-26 08:50:08 -0700

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: checker
+ * @s2: checker
+ *
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, j, num;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		for (j = 0; s2[j] != '\0'; j++)
+		{
+			num = s1 - s2;
+		}
+
+	}
+	return (num);
+}
