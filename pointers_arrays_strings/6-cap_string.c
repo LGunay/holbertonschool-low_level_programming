edde90cca566033116b3ef67a664355e10505ee0
root <gqasimova93@gmail.com>
2023-10-27 08:48:02 -0700

    6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* cap_string - prints a string
+* @s:pointer
+*
+* Return: Always 0.
+*/
+char *cap_string(char *s)
+{
+	int i;
+	int l = strlen(s);
+
+	for (i = 0; i < l; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122 && s[i - 1] < 48)
+		{
+			s[i] = s[i] - 32;
+		}
+		if (s[i] == '\t')
+		{
+			s[i] = ' ';
+		}
+	}
+	return (s);
+}
