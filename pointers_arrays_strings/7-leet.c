efe9802a954fd14f582c6334465f025fa18ddb93
root <gqasimova93@gmail.com>
2023-10-27 10:07:33 -0700

    7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+/**
+  * leet - reset to 98
+  *
+  * @s: str
+  * Return: length
+ */
+
+char *leet(char *s)
+{
+	int i = 0, j, temp;
+	char h[20];
+
+	h[0] = '4';
+	h[4] = '3';
+	h[11] = '1';
+	h[14] = '0';
+	h[19] = '7';
+
+	while (s[i])
+	{
+		if (s[i] == 'a' || s[i] == 'A' || s[i] == 'e' || s[i] == 'E' || s[i] == 'o'
+				|| s[i] == 'O' || s[i] == 'T' || s[i] == 't' || s[i] == 'L' || s[i] == 'l')
+		{
+			for (; s[i] > 90;)
+			{
+				s[i] -= 32;
+			}
+			temp = s[i] - 65;
+			s[i] = h[temp];
+		}
+		i++;
+	}
+	return (s);
+}
