5eddbfc26dcec7115e7b56e96bf701680068453d
root <gqasimova93@gmail.com>
2023-10-28 12:20:29 -0700

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * *_memset - prints buffer in hexa
+ * *_memcpy - prints buffer in hexa
  * @src: var
  * @dest: var
  * @n: integer
@@ -12,7 +12,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for ( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
