273b5099051e927ba926891ae5fb4ebea42cbbd9
root <gqasimova93@gmail.com>
2023-10-28 14:47:52 -0700

    3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strspn - check the code
+ * @s: pointer
+ * @accept: pointer
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int l = strlen(s);
+
+	for (i = 0; i < l; i++)
+	{
+		if (s == accept)
+	}
+	return (l);
+}
