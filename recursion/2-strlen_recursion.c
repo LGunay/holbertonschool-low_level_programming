fa8a23b1fae69c74c25b5d497f6fd926d503a00d
root <gqasimova93@gmail.com>
2023-10-30 08:57:12 -0700

    2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  _strlen_recursion - fills memory
+ * @s: data
+ *
+ * Return: char
+ */
+int _strlen_recursion(char *s)
+{
+	int i = strlen(s);
+	
+	return (i);
+}
