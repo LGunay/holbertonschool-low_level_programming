ac0c94283c032e6a746dd92a6170175f4e1a73d0
root <gqasimova93@gmail.com>
2023-11-01 06:10:36 -0700

    5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _sqrt_recursion - fills memory
+ * @n: data
+ *
+ *
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (-1);
+	return (sqrt(n, 1);
+}
+void sqrt (int x, int k)
+{
+	if (k > ((n / 2) + 1))
+		return (-1);
+	else if ((n / k) == k && (n % k) == 0)
+		return (k);
+	else
+		return (sqrt(n, k + 1));
+}
