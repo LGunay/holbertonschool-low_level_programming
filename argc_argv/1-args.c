1163f6b4fcd4f1fb28a81ea0426850d020987445
root <gqasimova93@gmail.com>
2023-11-04 07:01:47 -0700

    1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: integer
+ * @argv: char
+ * Return: Always 0.
+ */
+int main(int argc, char __attribute__((unused)) **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
