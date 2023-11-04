eddd4260dce6eb93507600f51e612a7607f8ffad
root <gqasimova93@gmail.com>
2023-11-04 11:20:28 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -6,16 +6,16 @@
  * @argv: char
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int i, j = 1;
 
 	if (argc <= 2)
 		printf("ERROR\n");
 	return (1);
-	for (i = 2; i <= argc; i++)
+	for (i = 1; i <= argc; i++)
 	{
-		j = j * atoi(argv[i]);
+		j = j * atoi(*(argv + i));
 	}
 	printf("%d\n", j);
 	return (0);
