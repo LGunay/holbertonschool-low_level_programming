c53ec0f92698dd981c5d545183d266bea14bf2a2
root <gqasimova93@gmail.com>
2023-11-04 11:11:10 -0700

    3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,14 +8,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, j = 1;
 
 	if (argc <= 2)
 		printf("ERROR\n");
 	return (1);
-	for (i = 1; i < argc; i++)
+	for (i = 1; i <= argc; i++)
 	{
-		j = atoi(argv[1]) * atoi(argv[i]);
+		j = j * atoi(argv[i]);
 	}
 	printf("%d\n", j);
 	return (0);
