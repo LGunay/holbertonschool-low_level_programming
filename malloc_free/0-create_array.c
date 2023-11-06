f0bdeef8039eb1d6ca556fd566e552558a03e25a
root <gqasimova93@gmail.com>
2023-11-06 06:44:15 -0800

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
+	{
+		printf("failed to allocate memory\n");
 		return ("NULL");
 	t = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
