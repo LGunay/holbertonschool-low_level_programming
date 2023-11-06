992a0637c4f7356681924c37722ddd9cdc8ae4e5
root <gqasimova93@gmail.com>
2023-11-06 07:30:00 -0800

    1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * create_array - creates an array
+ * @str: var
+ *
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	char *t;
+	int i, l = strlen(str);
+
+	if (str == NULL)
+		return (NULL);
+	t = malloc(sizeof(char) * l);
+	if (t == 0)
+		return (NULL);
+	for (i = 0; i < l; i++)
+	{
+		t[i] = str[i];
+	}
+	return (t);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,4 +1,5 @@
 #ifndef HH
 #define HH
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif
