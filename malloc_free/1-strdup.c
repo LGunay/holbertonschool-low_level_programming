052c0b7ae18b9565c1c08003499e673ae8d9c9e3
root <gqasimova93@gmail.com>
2023-11-06 07:38:55 -0800

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,10 +11,11 @@
 char *_strdup(char *str)
 {
 	char *t;
-	int i, l = strlen(str);
+	int i, l;
 
 	if (str == NULL)
 		return (NULL);
+	l = strlen(str);
 	t = malloc(sizeof(char) * l);
 	if (t == 0)
 		return (NULL);
