7d0d5b80704f45d3dc98076c47675936af78d48f
root <gqasimova93@gmail.com>
2023-11-06 07:40:13 -0800

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * create_array - creates an array
+ * _strdup - creates an array
  * @str: var
  *
  * Return: Always 0 (Success)
@@ -16,12 +16,13 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	l = strlen(str);
-	t = malloc(sizeof(char) * l);
+	t = malloc(sizeof(char) * l + 1);
 	if (t == 0)
 		return (NULL);
 	for (i = 0; i < l; i++)
 	{
 		t[i] = str[i];
 	}
+	t[i] = '\0';
 	return (t);
 }
