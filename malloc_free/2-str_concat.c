9732a8fd661edcbf120d3154367f48b7c0c44364
root <gqasimova93@gmail.com>
2023-11-09 05:48:25 -0800

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,8 +13,8 @@ char *str_concat(char *s1, char *s2)
 	char *t;
 	int i, l, n;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL || s2 == NULL)
+		return (1);
 	l = strlen(s1) + strlen(s2);
 	n = strlen(s1);
 	t = malloc(sizeof(char) * l + 1);
