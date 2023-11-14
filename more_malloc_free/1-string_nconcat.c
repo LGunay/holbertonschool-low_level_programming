55d8440eb20fc0c59ba70fb4b3eac64b1dacc38e
root <gqasimova93@gmail.com>
2023-11-14 05:55:41 -0800

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, x, y;
 	char *m;
 
-	i = strlen(s1);
-	x = strlen(s2);
+	if (s1 == NULL)
+		i = 0;
+	else
+		i = strlen(s1);
+	if (s2 == NULL)
+		x = 0;
+	else
+		x = strlen(s2);
 	if (n >= x)
 		n = x;
 	y = i + n;
