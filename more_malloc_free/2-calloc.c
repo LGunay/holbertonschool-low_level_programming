eedb45347815a7e9dcabcd6a7d5235230928daaa
root <gqasimova93@gmail.com>
2023-11-14 06:40:24 -0800

    2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _calloc - fills memory
+ * @nmemb: tr
+ * @size: str
+ * Return: char
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *m;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	m = malloc(nmemb * size);
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+		m[i] = 0;
+	}
+	return(m);
+}	
