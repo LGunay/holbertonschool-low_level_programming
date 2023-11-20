09e583d7278ef026aff9d1f27634303521ce5280
root <gqasimova93@gmail.com>
2023-11-20 05:04:33 -0800

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef HH
 #define HH
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
