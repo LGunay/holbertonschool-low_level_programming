9a19604f2f2421ade71d870c0b9f02e00eaf7f2f
root <gqasimova93@gmail.com>
2023-11-20 06:21:18 -0800

    2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - check the code
+ * @d:pointer
+ * @name:name
+ * @age:age
+ * @owner:owner
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if ((*d).name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", (*d).name);
+	if ((*d).age >= 0)
+		printf("Age: %f\n", (*d).age);
+	if ((*d).owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n",(*d).owner);
+}
