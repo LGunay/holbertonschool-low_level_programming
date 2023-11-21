9377586158196e19036ea4bbd9faf444c1d77f21
root <gqasimova93@gmail.com>
2023-11-21 09:36:38 -0800

    2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * int_index - init
+  * @array: array
+  * @size: size
+  * @cmp: action
+  * Return: return
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL)
+		return (0);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
