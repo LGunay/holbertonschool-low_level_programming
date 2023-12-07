8b123b77caa92e0c90b451af65be145bb56f8419
root <gqasimova93@gmail.com>
2023-12-07 07:51:04 -0800

    4

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: var
+ *
+ * Return: Always 0.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *point;
+
+	while (head)
+	{
+		point = head;
+		head = head->next;
+	}
+	free (point);
+}	
