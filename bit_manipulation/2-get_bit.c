414faf9ac0cb9768af74ec50adc0617b3e342e1e
root <gqasimova93@gmail.com>
2023-12-10 04:47:28 -0800

    2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -5,13 +5,9 @@
   * @index: index of bit which this function returns
   * Return: the value of a bit at a given index. if error ocured -1
   */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int k = 1;
-
 	if (index > 31)
 		return (-1);
-	k = k << index;
-	*n |= k;
-	return (1);
+	return ((n >> index) & 1);
 }
