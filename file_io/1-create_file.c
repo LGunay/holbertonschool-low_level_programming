6c28562f1875fdde4c38db08a9b01b27c2258292
root <gqasimova93@gmail.com>
2023-12-12 07:41:26 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,7 +22,6 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 	{
 		text_content = "";
-		return (1);
 	}
 	w = write(fd, text_content, len);
 	if (w == -1 || fd == -1)
