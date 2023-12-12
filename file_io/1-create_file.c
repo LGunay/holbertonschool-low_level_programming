a0a2c94b4a9fa7c65feed495c65bd1a6c09c6bc6
root <gqasimova93@gmail.com>
2023-12-12 07:28:47 -0800

    1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,7 +13,7 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, c, w, len;
+	int fd, w, len;
 
 	len = strlen(text_content);
 	if (!filename)
@@ -21,7 +21,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (!text_content)
 	{
-		text_content = "";
+		fd = creat (filename, 0600);
+		return (1);
 	}
 	w = write(fd, text_content, len);
 	if (w == -1 || fd == -1)
