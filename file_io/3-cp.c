b161f007eb014035e707fa9e44be22a51d50ec44
root <gqasimova93@gmail.com>
2023-12-13 12:35:07 -0800

    3

---
+++ file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#define BUFFER_SIZE 1024
+
+/**
+ * copier - copy the content of first argument to another.
+ *
+ * @file_from: source file.
+ *
+ * @file_to: destination file.
+ */
+void copyfile(const char *file_from, const char *file_to)
+{
+	int fdf, fdt, w, r = 1;
+	char buffersize [1024];
+	char *buff;
+
+	buff = malloc(buffersize);
+	fdf = open(file_from,O_RDONLY);
+	fdt = open(file_to, O_TRUNC | O_RDONLY | O_CREAT, 0664);
+	if (fdf == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", file_from);
+		exit(98);
+	}
+	while(r > 0)
+	{
+		r = read(fdf, buff, buffersize);
+		w = write(fdt, buff, fdf);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", file_from);
+			exit(98);
+		}
+		if (fdt == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to%s\n", file_to);
+		}
+	}
+	close(fdt);
+	if (close(fdt) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdt);
+		exit(100);
+	}
+}
+/**
+ * main - main block.
+ *
+ * @argc: count of arguments.
+ *
+ * @argv: argument vector.
+ *
+ * Return: always 0.
+ */
+int main(int argc, char **argv)
+{
+	char *file_from, *file_to;
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	file_from = argv[1];
+	file_to = argv[2];
+	copyfile(file_from, file_to);
+	return (0);
+}
