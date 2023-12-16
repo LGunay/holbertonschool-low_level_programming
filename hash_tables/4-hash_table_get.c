53c2d821d03f2b28d6f2146c5ca14e8d02fdef86
root <gqasimova93@gmail.com>
2023-12-16 08:00:31 -0800

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -5,7 +5,6 @@
   * hash_table_get - adds an element to the hash table.
   * @ht: hash_table
   * @key: key
-  * @value: value
   * Return:  actual number of letters it could read and print
   */
 char *hash_table_get(const hash_table_t *ht, const char *key)
