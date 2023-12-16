28735c79738e7456565e2f1fe33ad7a5943a034d
root <gqasimova93@gmail.com>
2023-12-16 08:10:16 -0800

    4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	temp = ht->array[index];
 	if (!temp)
 		return (NULL);
-	for (i = 0; i == index; i++)
+	for (i = 0; i < index; i++)
 	{
 		if (!strcmp(temp->key, key))
 		{
