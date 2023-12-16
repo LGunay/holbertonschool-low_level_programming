4ccb3c4668e9500b587214949b316dabbd429c27
root <gqasimova93@gmail.com>
2023-12-16 10:22:02 -0800

    4

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (k)
 				printf(", ");
-			printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			if (!k)
 				k++;
 		}
