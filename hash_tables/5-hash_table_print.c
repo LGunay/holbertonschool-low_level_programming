6310ea315549d2fca40877bf85c2fbcbc0b7f5a2
root <gqasimova93@gmail.com>
2023-12-16 10:36:19 -0800

    4

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -10,6 +10,7 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, len, k = 0;
+	hash_node_t *temp;
 
 	if (!ht)
 		return;
@@ -19,11 +20,16 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[i])
 		{
-			if (k)
-				printf(", ");
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-			if (!k)
-				k++;
+			temp = ht->array[i];
+			while (temp)
+			{
+				if (k)
+					printf(", ");
+				printf("'%s': '%s'", temp->key, temp->value);
+				if (!k)
+					k++;
+				temp = temp->next;
+			}
 		}
 	}
 	printf("}");
