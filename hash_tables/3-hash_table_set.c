5f2447a8741335a97629d5356d0414bccf3b7d86
root <gqasimova93@gmail.com>
2023-12-16 11:12:19 -0800

    6

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -31,6 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		do {
 			if (!strcmp(temp->key, key))
 			{
+				free(temp->value);
 				temp->value = strdup(value);
 				return (1);
 			}
