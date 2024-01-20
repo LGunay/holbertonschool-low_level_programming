9fece28a57f40dea18102e2f66639af08d4db15a
root <gqasimova93@gmail.com>
2024-01-20 10:42:27 -0800

    fixed space

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -18,7 +18,7 @@ int binary_search(int *array, size_t size, int value)
 	while (low <= high)
 	{
 		mid = low + (high - low) / 2;
-		printf("Searching in array:");
+		printf("Searching in array: ");
 		for (i = low; i < high + 1; i++)
 		{
 			printf("%d", array[i]);
