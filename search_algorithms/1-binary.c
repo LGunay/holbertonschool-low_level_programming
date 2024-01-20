a7bbba212d8bcc2aa041a6e66f61ab105c78b290
root <gqasimova93@gmail.com>
2024-01-20 10:40:18 -0800

    using the Binary search algorithm

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+#include <stdio.h>
+/**
+ * binary_search -  Binary search algorithm
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: return
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid, low, high, i;
+
+	if (!array)
+		return (-1);
+	low = 0;
+	high = size - 1;
+	while (low <= high)
+	{
+		mid = low + (high - low) / 2;
+		printf("Searching in array:");
+		for (i = low; i < high + 1; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+		}
+		printf("\n");
+		if (value < array[mid])
+			high = mid - 1;
+		else if (value > array[mid])
+			low = mid + 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -2,4 +2,5 @@
 #define Search
 #include <stddef.h>
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 #endif
